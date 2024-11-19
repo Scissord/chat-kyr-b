05a8d3946a12555e4343f55923afb010bac68368
nurdaulet <scissxrd@gmail.com>
2024-11-19 17:17:07 +0500

    fix

--- services/greenApi/sendFileMessage.js
+++ services/greenApi/sendFileMessage.js
@@ -3,12 +3,24 @@ import * as Message from '../../models/message.js';
 import * as Attachment from '../../models/attachment.js';
 import * as Instance from '../../models/instance.js';
 import * as User from '../../models/user.js';
+import * as Customer from '../../models/customer.js';
+import randomInstance from '../instance/randomInstance.js';
 
 export default async function sendFileMessage(user_id, customer, file, customer_id) {
-  const instance = await Instance.findByBuyerPhone(customer.buyer_phone);
-  const url = process.env.URL + 'uploads/' + file.filename;
+  let instance = await Instance.findByBuyerPhone(customer.buyer_phone);
+  if (!instance) {
+    const { randomBuyerPhone, randomInstanceId, randomApiToken } = await randomInstance();
+    instance = {
+      instance_id: randomInstanceId,
+      api_token: randomApiToken,
+      phone: randomBuyerPhone
+    };
 
-  console.log(file);
+    await Customer.update(customer_id, {
+      buyer_phone: randomBuyerPhone
+    });
+  }
+  const url = process.env.URL + 'uploads/' + file.filename;
 
   const res = await axios({
     url: `${process.env.GREEN_API_URL}/waInstance${instance.instance_id}/sendFileByUrl/${instance.api_token}`,
